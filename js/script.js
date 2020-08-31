51716bdd51e810973fc65d6e529f7f8753134993
Vlad-Polyakov-24 <vladon24121995@gmail.com>
2020-09-01 00:07:32 +0300

    FINISH

--- js/script.js
+++ js/script.js
@@ -1,27 +1,37 @@
 /*BURGER*/
-$('.header__menu-icon').click(function(event){
-	$(this).toggleClass('active');
-	$('.header__menu').toggleClass('active');
-	$('body').toggleClass('lock');
-});
-/*BURGER-END*/
-$(document).ready(function(){
-	$('.slider').slick({
-		arrows:false,
-		dots:true,
-		slidesToShow:1,
-		autoplay:true,
-		speed:500,
-		autoplaySpeed:1500,
-		infinite: true,
-		cssEase: 'linear',
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:1
-				}
-			}
-		]
+	$('.header__menu-icon').click(function(event){
+		$(this).toggleClass('active');
+		$('.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
 	});
-});
+
+	$('.header__menu-link').click(function(event){
+		$('body').removeClass('lock');
+		$('.header__menu').removeClass('active');
+		$('.header__menu-icon').removeClass('active');
+	});
+
+/*=========*/
+
+/*SLIDER*/
+	$(document).ready(function(){
+		$('.slider').slick({
+			arrows:false,
+			dots:true,
+			slidesToShow:1,
+			autoplay:true,
+			speed:500,
+			autoplaySpeed:1500,
+			infinite: true,
+			cssEase: 'linear',
+			responsive:[
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow:1
+					}
+				}
+			]
+		});
+	});
+/*=========*/
